51417f14ef01cb7080712dc4e2db72eb292ae788
fi.x64 <hohoangphia@gmail.com>
2022-10-07 23:32:11 +0700

    Fix user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -108,7 +108,7 @@ let createNewUser = (data) => {
             if (check) {
                 resolve({
                     errCode: 1,
-                    message: 'Your email is already in use. Please try another email!'
+                    errMessage: 'Your email is already in use. Please try another email!'
                 })
             } else {
                 let hashPasswordFromBcrypt = await hashUserPassword(data.password);
