b13fde9f052bfa6b15a6114cced3531f884b245b
fi.x64 <hohoangphia@gmail.com>
2022-10-26 03:07:59 +0700

    Add associations

--- src/controller/doctorController.js
+++ src/controller/doctorController.js
@@ -2,15 +2,15 @@ import doctorService from '../services/doctorService';
 
 let getTopDoctorHome = async (req, res) => {
     let limit = 10;
-    
-    if(req.query.limit){
+
+    if (req.query.limit) {
         limit = req.query.limit;
     }
 
     try {
-        let response = await doctorService.getTopDoctorHomeService(+limit);
+        let response = await doctorService.getTopDoctorHome(+limit);
         return res.status(200).json(response);
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         return res.status(200).json({
             errCode: -1,
@@ -19,6 +19,46 @@ let getTopDoctorHome = async (req, res) => {
     }
 }
 
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error code from server...'
+        })
+    }
+}
+
+let postInforDoctor = async (req, res) => {
+    try {
+        let response = await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+let getDetailDoctorById = async (req, res) => {
+    try {
+        let infor = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    postInforDoctor: postInforDoctor,
+    getDetailDoctorById: getDetailDoctorById,
 }
