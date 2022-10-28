c894ae543eb28be8649e2dd3059868bee676de6a
fi.x64 <hohoangphia@gmail.com>
2022-10-29 03:11:36 +0700

    Add dynamic data for manage doctor's schedule

--- src/controller/doctorController.js
+++ src/controller/doctorController.js
@@ -57,9 +57,22 @@ let getDetailDoctorById = async (req, res) => {
 }
 
 let bulkCreateSchedule = async (req, res) => {
-    console.log("Check req.body: ", req.body);
     try {
         let infor = await doctorService.bulkCreateSchedule(req.body);
+        return res.status(200).json({
+            infor
+        })
+    } catch (e) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
+let getScheduleByDate = async (req, res) => {
+    try {
+        let infor = await doctorService.getScheduleByDate(req.query.doctorId, req.query.date);
         return res.status(200).json({
             infor
         })
@@ -77,4 +90,5 @@ module.exports = {
     postInforDoctor: postInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
     bulkCreateSchedule: bulkCreateSchedule,
+    getScheduleByDate: getScheduleByDate
 }
