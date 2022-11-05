522efee55835b2964c0b515a624114de8896bf21
fi.x64 <hohoangphia@gmail.com>
2022-11-05 23:08:34 +0700

    Edit clinic modal and add clnic to db

--- src/migrations/migration-cerate-clinic.js
+++ src/migrations/migration-cerate-clinic.js
@@ -14,11 +14,14 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
-      description: {
-        type: Sequelize.STRING
+      descriptionMarkdown: {
+        type: Sequelize.TEXT
+      },
+      descriptionHTML: {
+        type: Sequelize.TEXT
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.BLOB('long')
       },
       createdAt: {
         allowNull: false,
